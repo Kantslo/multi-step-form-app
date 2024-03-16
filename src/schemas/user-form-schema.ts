2078dda0551be814c69c3d9f8018f1b0010f3671
Kantslo <gqancliani@gmail.com>
2024-03-17 03:10:08 +0400

    chore: slight adjustment to user form schema

--- src/schemas/user-form-schema.ts
+++ src/schemas/user-form-schema.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 
 export const userFormSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1, {message: 'Name is required'}),
   email: z.string().email(),
   phone: z.string().min(6).max(15),
 });
