c7bc5f61b3ac34429593a5c4e24d3dbd37424b90
Kantslo <gqancliani@gmail.com>
2024-03-18 01:32:15 +0400

    updated react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import PlanForm from "./pages/PlanForm";
-import SidebarMobile from "./components/SidebarMobile";
 import UserForm from "./pages/UserForm";
-import useMultiStepForm from "./components/useMultiStepForm"
 import AddonForm from "./pages/AddonForm";
 import Finish from "./pages/Finish";
 
 function App() {
-  const { step, isFirstStep, isLastStep, nextStep, prevStep } = useMultiStepForm([
-    <UserForm />,
-    <PlanForm />,
-    <AddonForm />,
-    <Finish />
-  ]);
-
   return (
     <>
-      <div className="bg-[#EFF5FF] min-h-screen flex flex-col justify-between">
-        <div>
-          <SidebarMobile />
-          {step}
-        </div>
-          <div style={{ justifyContent: isFirstStep ? 'flex-end' : 'space-between' }} className="flex flex-row items-center bg-white h-[72px] px-4 w-[100%]">
-            {!isFirstStep && <button className="bg-[#022959] rounded-[4px] px-4 py-3 text-white" type="button" onClick={prevStep}>Go Back</button>}
-            <button className="bg-[#022959] rounded-[4px] px-4 py-3 text-white" type="button" onClick={nextStep}>{isLastStep ? "Confirm" : "Next Step"}</button>
-          </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/user" />} />
+        <Route path="/user" element={<UserForm />} />
+        <Route path="/plan" element={<PlanForm />} />
+        <Route path="/addons" element={<AddonForm />} />
+        <Route path="/finish" element={<Finish />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.tsx";
+import NotFoundPage from "./components/NotFoundPage.tsx";
+import UserForm from "./pages/UserForm.tsx";
+import PlanForm from "./pages/PlanForm.tsx";
+import AddonForm from "./pages/AddonForm.tsx";
+
+import "./index.css";
+import Finish from "./pages/Finish.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/user",
+    element: <UserForm />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/plan",
+    element: <PlanForm />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/addons",
+    element: <AddonForm />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/finish",
+    element: <Finish />,
+    errorElement: <NotFoundPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
