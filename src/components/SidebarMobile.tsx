38420f559494145fa1bb1d6ef7ef85edd0a06bd0
Kantslo <gqancliani@gmail.com>
2024-03-19 02:43:18 +0400

    feat: implemented zustand store and finished routing

--- src/components/SidebarMobile.tsx
+++ src/components/SidebarMobile.tsx
@@ -16,7 +16,7 @@ const SidebarMobile = () => {
               border: Number === 1 ? "none" : "",
             }}
             onClick={() => setPage(1)}
-            className={`font-[0.875rem] text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
+            className={`text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
             to="/user">
             1
           </Link>
@@ -27,7 +27,7 @@ const SidebarMobile = () => {
               border: Number === 2 ? "none" : "",
             }}
             onClick={() => setPage(2)}
-            className={`font-[0.875rem] text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
+            className={`text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
             to="/plan">
             2
           </Link>
@@ -38,7 +38,7 @@ const SidebarMobile = () => {
               border: Number === 3 ? "none" : "",
             }}
             onClick={() => setPage(3)}
-            className={`font-[0.875rem] text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
+            className={`text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
             to="/addons">
             3
           </Link>
@@ -49,7 +49,7 @@ const SidebarMobile = () => {
               border: Number === 4 ? "none" : "",
             }}
             onClick={() => setPage(4)}
-            className={`font-[0.875rem] text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
+            className={`text-white font-bold border border-white rounded-full w-[2.063rem] h-[2.063rem] flex items-center justify-center`}
             to="/finish">
             4
           </Link>
