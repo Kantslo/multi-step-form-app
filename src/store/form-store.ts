156ccbf8c087535d851bd7ddc6768430a97f853b
Kantslo <gqancliani@gmail.com>
2024-03-19 00:05:13 +0400

    feat: created zustand store for forms

---
+++ src/store/form-store.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+type FormType = {
+  name: string;
+  email: string;
+  phone: string;
+  plan: string | null;
+  billing: string | null;
+  addons: string | null;
+};
+
+const form: FormType = {
+  name: "",
+  email: "",
+  phone: "",
+  plan: null,
+  billing: null,
+  addons: null,
+};
+
+type Store = {
+  form: FormType;
+  setForm: (form: Partial<FormType>) => void;
+};
+
+export const useFormStore = create<Store>((set) => ({
+  form: form,
+  setForm: (form: Partial<FormType>) =>
+    set((state: any) => ({ form: { ...state.form, ...form } })),
+  getStorage: () => localStorage,
+}));
